140da399adf6180a94b9e6e9369e69a3435c24c7
Alison James <alison.james@cbsa-asfc.gc.ca>
2020-03-26 15:43:59 -0400

    Initial commit

---
+++ H3044-P_RPP/fr/story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5tKZflttLmL":
+        Script1();
+        break;
+      case "5b1VxA1D1zC":
+        Script2();
+        break;
+      case "6F9XhjgJrSM":
+        Script3();
+        break;
+      case "6caas0H6iXL":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("completed")
+}
+
+function Script2()
+{
+  SetStatus("completed")
+}
+
+function Script3()
+{
+  SetStatus("completed")
+}
+
+function Script4()
+{
+  SetStatus("completed")
+}
+
