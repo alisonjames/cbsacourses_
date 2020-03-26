da99ce59d7d7736325665facaf012703dc0b329e
Alison James <alison.james@cbsa-asfc.gc.ca>
2020-03-26 15:55:13 -0400

    New try...

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6rdqIaknCbI":
+        Script1();
+        break;
+      case "5XCt91F3Jt9":
+        Script2();
+        break;
+      case "6RcScLdHpjC":
+        Script3();
+        break;
+      case "6aQlhEHZ1ST":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  SetStatus("completed")
+}
+
+function Script2()
+{
+  SetStatus("completed")
+}
+
+function Script3()
+{
+  SetStatus("completed")
+}
+
+function Script4()
+{
+  SetStatus("completed")
+}
+
